0b5a0eba418f5740601e97e413de91f249cd8122
codernocook <90613458+codernocook@users.noreply.github.com>
2023-05-30 08:11:37 +0100

    Update client.js

--- client.js
+++ client.js
@@ -4,100 +4,102 @@
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 module.exports = function(db, authkey) {
-    function set(key, value, callback) {
-        try {
-            if (db) {
-                let bodyfetch = {
-                    "authorization": authkey,
-                    "key": key,
-                    "value": value
-                }
-                fetch(db + "/set", { method: "POST", body: JSON.stringify(bodyfetch), headers: { 'Content-Type': 'application/json' }}).then(res => res.json()).then(json => {
-                    if (json) {
-                        if (json["status"] === false) return callback(false);
-                        if (json["status"] === true) return callback(json);
+    return {
+        set(key, value, callback) {
+            try {
+                if (db) {
+                    let bodyfetch = {
+                        "authorization": authkey,
+                        "key": key,
+                        "value": value
                     }
-                })
-            }
-        } catch {
-            return callback(false);
-        }
-    };
-    function has(key, callback) {
-        try {
-            if (db) {
-                let bodyfetch = {
-                    "authorization": authkey,
-                    "key": key
-                }
-                fetch(db + "/has", { method: "POST", body: JSON.stringify(bodyfetch), headers: { 'Content-Type': 'application/json' }}).then(res => res.json()).then(json => {
-                    if (json) {
-                        if (json["status"] === false) return callback(false);
-                        if (json["status"] === true) return callback(true);
-                    }
-                })
-            }
-        } catch {
-            return callback(false);
-        }
-    };
-    function get(key, callback) {
-        try {
-            if (db) {
-                let bodyfetch = {
-                    "authorization": authkey,
-                    "key": key
-                }
-                fetch(db + "/has", { method: "POST", body: JSON.stringify(bodyfetch), headers: { 'Content-Type': 'application/json' }}).then(res => res.json()).then(json => {
-                    if (json) {
-                        if (json["status"] === false) return callback(false);
-    
-                        if (json["data"]) {
-                            return callback(json["data"]);
-                        } else {
-                            return callback(undefined);
+                    fetch(db + "/set", { method: "POST", body: JSON.stringify(bodyfetch), headers: { 'Content-Type': 'application/json' }}).then(res => res.json()).then(json => {
+                        if (json) {
+                            if (json["status"] === false) return callback(false);
+                            if (json["status"] === true) return callback(json);
                         }
+                    })
+                }
+            } catch {
+                return callback(false);
+            }
+        },
+        has(key, callback) {
+            try {
+                if (db) {
+                    let bodyfetch = {
+                        "authorization": authkey,
+                        "key": key
                     }
-                })
+                    fetch(db + "/has", { method: "POST", body: JSON.stringify(bodyfetch), headers: { 'Content-Type': 'application/json' }}).then(res => res.json()).then(json => {
+                        if (json) {
+                            if (json["status"] === false) return callback(false);
+                            if (json["status"] === true) return callback(true);
+                        }
+                    })
+                }
+            } catch {
+                return callback(false);
             }
-        } catch {
-            return callback(undefined);
-        }
-    };
-    function del(key, callback) {
-        try {
-            let bodyfetch = {
-                "authorization": authkey,
-                "key": key
-            }
-            if (db) {
-                fetch(db + "/del", { method: "DELETE", body: JSON.stringify(bodyfetch), headers: { 'Content-Type': 'application/json' }}).then(res => res.json()).then(json => {
-                    if (json) {
-                        if (json["status"] === false) return callback(false);
-                        if (json ["status"] === true) return callback(json);
+        },
+        get(key, callback) {
+            try {
+                if (db) {
+                    let bodyfetch = {
+                        "authorization": authkey,
+                        "key": key
                     }
-                })
+                    fetch(db + "/has", { method: "POST", body: JSON.stringify(bodyfetch), headers: { 'Content-Type': 'application/json' }}).then(res => res.json()).then(json => {
+                        if (json) {
+                            if (json["status"] === false) return callback(false);
+        
+                            if (json["data"]) {
+                                return callback(json["data"]);
+                            } else {
+                                return callback(undefined);
+                            }
+                        }
+                    })
+                }
+            } catch {
+                return callback(undefined);
             }
-        } catch {
-            return callback(false);
+        },
+        del(key, callback) {
+            try {
+                let bodyfetch = {
+                    "authorization": authkey,
+                    "key": key
+                }
+                if (db) {
+                    fetch(db + "/del", { method: "DELETE", body: JSON.stringify(bodyfetch), headers: { 'Content-Type': 'application/json' }}).then(res => res.json()).then(json => {
+                        if (json) {
+                            if (json["status"] === false) return callback(false);
+                            if (json ["status"] === true) return callback(json);
+                        }
+                    })
+                }
+            } catch {
+                return callback(false);
+            }
+        },
+        clear(key, callback) {
+            try {
+                let bodyfetch = {
+                    "authorization": authkey,
+                    "key": key
+                }
+                if (db) {
+                    fetch(db + "/clear", { method: "POST", body: JSON.stringify(bodyfetch), headers: { 'Content-Type': 'application/json' }}).then(res => res.json()).then(json => {
+                        if (json) {
+                            if (json["status"] === false) return callback(false);
+                            if (json ["status"] === true) return callback(json);
+                        }
+                    })
+                }
+            } catch {
+                return callback(false);
+            }
         }
-    };
-    function clear(key, callback) {
-        try {
-            let bodyfetch = {
-                "authorization": authkey,
-                "key": key
-            }
-            if (db) {
-                fetch(db + "/clear", { method: "POST", body: JSON.stringify(bodyfetch), headers: { 'Content-Type': 'application/json' }}).then(res => res.json()).then(json => {
-                    if (json) {
-                        if (json["status"] === false) return callback(false);
-                        if (json ["status"] === true) return callback(json);
-                    }
-                })
-            }
-        } catch {
-            return callback(false);
-        }
-    };
+    }
 }
